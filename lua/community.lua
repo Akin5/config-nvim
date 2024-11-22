12325e36f0baec49363c828ca9b91a508bf383ad
Akin5 <akin@programmer.net>
2024-11-22 11:33:32 +0700

    config nvim gwe

---
+++ lua/community.lua
@@ -0,0 +1,24 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  -- { import = "astrocommunity.completion.codeium-nvim" },
+  -- { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.indent.indent-rainbowline" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.ultimate-autopair-nvim" },
+  { import = "astrocommunity.utility.hover-nvim" },
+}
